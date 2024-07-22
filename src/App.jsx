88f9bbea1202f6cc6ea6b0ca284460f23ca403d0
Tetiana Hrihorash <tmgrigorash@gmail.com>
2024-07-22 21:25:29 +0300

    del folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
 import { Heading } from "./components/Heading/Heading";
 import Profile from "./components/Profile/Profile";
 import userData from "./data/userData.json";
-import "./data/friendData.json";
-import FriendList from "./components/Friendlist/Friendlist";
+
+import transactions from "./data/transactions.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
 const App = () => {
   return (
     <>
-     <Heading title="Task 1 Profile Card" bottom />
+      <Heading title="Task 1 Profile Card" bottom />
       <Profile
         name={userData.username}
         tag={userData.tag}
@@ -18,8 +17,9 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
-       <Heading title="Task 2 FriendList " bottom />
-     <FriendList friendData={friendData}/>
+      <Heading title="Task 2 FriendList " bottom />
+      <Heading title="Task 3 Transaction history" bottom />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,10 @@
-
 import css from "./Profile.module.css";
 
-function Profile({name, tag, location, image, stats }) {
+function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.profile}>
       <div>
-        <img
-          className={css.profileImg}
-          src={image}
-          alt="User avatar"/>
+        <img className={css.profileImg} src={image} alt="User avatar" />
         <p className={css.profileUsername}>{name}</p>
         <p className={css.profileTag}>@{tag}</p>
         <p className={css.profileTag}>{location}</p>
